97ffd6e7e599125f9c479a99bfb55049a6c04c6d
huangxingyuan <uestchxy@163.com>
2020-11-12 22:41:00 +0800

    pathSum/求路径和

---
+++ tree/countNodes.js
@@ -0,0 +1,21 @@
+// 给出一个完全二叉树，求出该树的节点个数。
+
+//说明：
+
+//完全二叉树的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+// 参考：https://leetcode-cn.com/problems/count-complete-tree-nodes/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-47/
+var countNodes = function(root) {
+    if (!root) return 0;
+    let left = height(root.left);
+    let right = height(root.right);
+    if (left === right) {
+        return countNodes(root.right) + (1 << left); // 左右高度相等，左子树为满二叉树，节点数量为2^h - 1，加上根节点为2^h，再递归右子树
+    } else {
+        return countNodes(root.left) + (1 << right); // 高度不相等，右子树为满二叉树
+    }
+};
+
+function height(root) {
+    return !root ? 0 : 1 + height(root.left);
+}
+
---
+++ tree/pathSum.js
@@ -0,0 +1,24 @@
+// 给定一棵二叉树，其中每个节点都含有一个整数数值(该值或正或负)。
+//设计一个算法，打印节点数值总和等于某个给定值的所有路径的数量。
+// 注意，路径不一定非得从二叉树的根节点或叶节点开始或结束，但是其方向必须向下(只能从父节点指向子节点方向)。
+
+// 双DFS，每个结点都调用一次DFS
+var pathSum = function(root, sum) {
+
+    let count = 0
+    function travel(root,sum){
+        if(!root) return 
+        if(root.val === sum){
+            count++
+        }
+        travel(root.left,sum-root.val)
+        travel(root.right,sum-root.val)
+    }
+    function preOrder(root){
+        travel(root.val,sum)
+        preOrder(root.left)
+        preOrder(root.right)
+    }
+    preOrder(root)
+    return count
+};
