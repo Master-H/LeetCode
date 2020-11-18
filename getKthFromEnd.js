413a0e3d6fd5f4dbd855388d8580434fbc1cf418
huangxingyuan <uestchxy@163.com>
2020-11-18 23:47:36 +0800

    kthToLast/倒数k个节点

--- getKthFromEnd.js
+++ getKthFromEnd.js
@@ -2,9 +2,9 @@
 //题目： 链表中倒数第k个节点
 //关键： head不存在，链表长度小于k导致程序崩溃的边界条件
 // 栈方法
-function getKthFromEnd(head,k) {
+function kthToLast(head,k) {
   let stacks = []
-  let ans = []
+  let ans = null
   while(head){
     stacks.push(head)
     head = head.next
@@ -13,15 +13,14 @@ function getKthFromEnd(head,k) {
     ans = stacks.pop()
     k--
   }
-  return ans
+  return ans.val
   
 }
 
 // 链表方法, 倒数第k个，就是正数的第n-k+1,也就是正数走k次,可以画图
-var getKthFromEnd = function (head, k) {
+var kthToLast = function (head, k) {
   let first = head
   let second = head
-  if(!head) return null
   while( k >0){
     first = first.next
     k--
@@ -30,8 +29,22 @@ var getKthFromEnd = function (head, k) {
     first=first.next
     second=second.next
   }
-  return second
+  return second.val
 };
+var kthToLast = function(head, k) {
+  // 双指针
+  var left = head, right = head;
+  while(k > 0) {
+      right = right.next;
+      k--;
+  }
+  while(right !== null) {
+      left = left.next;
+      right = right.next;
+  }
+  return left.val
 
+
+};
 
 
