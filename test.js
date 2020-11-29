d62f8ee4c41b696eaa175c240753d9c092f2e994
huangxingyuan <uestchxy@163.com>
2020-11-29 15:56:50 +0800

    maxRepeating/最长重复字符串

--- test.js
+++ test.js
@@ -1,4 +1,22 @@
-console.log(15/10|0);
-console.log(15/10||0);
-console.log(5/10|0);
-console.log(5/10||0);
+var maxRepeating = function(str1, str2) {
+    let shortString = str1.length > str2.length ? str2 : str1
+    let longString = str1.length > str2.length ? str1 : str2
+    let shortLength = shortString.length
+    let res = ''
+    // 遍历取最大重复长度为i
+    for(let i = shortLength; i>0; i--){
+        // 最大重复长度为i,存在shortLength -i种可能
+        for(let j =0; j <=shortLength -i;j++){
+            let res =  shortString.substr(j,i)
+            if(longString.includes(repeatPart)) return res 
+            
+        }
+        
+    }
+    return res
+};
+
+
+let str1 = "ababc"
+let str2 = 'ab'
+console.log(maxRepeating(str1,str2));
