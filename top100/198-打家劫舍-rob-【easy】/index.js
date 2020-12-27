f16f88a1cf8091cafd792fa30e335ae659d153a0
huangxingyuan <uestchxy@163.com>
2020-12-27 22:34:20 +0800

    461-汉明距离-hammingDistance-【easy】

--- top100/198-打家劫舍-rob-【easy】/index.js
+++ top100/198-打家劫舍-rob-【easy】/index.js
@@ -2,7 +2,7 @@
  * @Author: huangxingyuan
  * @Date: 2020-12-27 20:30:45
  * @LastEditors: huangxingyuan
- * @LastEditTime: 2020-12-27 21:12:47
+ * @LastEditTime: 2020-12-27 21:56:19
  * @Description: 文件功能描述
  */
 // 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
@@ -29,10 +29,9 @@ var rob = function(nums) {
     let len = nums.length
     if(len === 0) return 0
     if(len === 1) return nums[0]
-    if(len === 2) return Math.max(nums[0],nums[1])
     let first = nums[0]
     let second = Math.max(nums[0],nums[1])
- 
+    if(len === 2) second 
     for(let i = 2; i < nums.length; i++){
         let temp = second
          second = Math.max(first+ nums[i],second)
