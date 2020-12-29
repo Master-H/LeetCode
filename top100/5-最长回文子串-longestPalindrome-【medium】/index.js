71eba8718ea4ad582f8fa19fc7702c350c88f8e4
huangxingyuan <uestchxy@163.com>
2020-12-29 21:50:45 +0800

    5-最长回文子串-longestPalindrome-【medium】

--- top100/5-最长回文子串-longestPalindrome-【medium】/index.js
+++ top100/5-最长回文子串-longestPalindrome-【medium】/index.js
@@ -2,7 +2,7 @@
  * @Author: huangxingyuan
  * @Date: 2020-12-20 12:36:34
  * @LastEditors: huangxingyuan
- * @LastEditTime: 2020-12-20 18:44:59
+ * @LastEditTime: 2020-12-29 21:50:23
  * @Description: 文件功能描述
  */
 // 给定一个字符串 s，找到 s 中最长的 子串。你可以假设 s 的最大长度为 1000。
@@ -14,20 +14,34 @@
 // 示例 2：
 // 输入: "cbbd"
 // 输出: "bb"
+// 解题思路：
+// 状态定义
+// dp[i,j]：字符串s从索引i到j的子串是否是回文串
+// true： s[i,j] 是回文串
+// false：s[i,j] 不是回文串
+// 转移方程
+// dp[i][j] = dp[i+1][j-1] && s[i] == s[j]
+// s[i] == s[j]：说明当前中心可以继续扩张，进而有可能扩大回文串的长度
+// dp[i+1][j-1]：true
+// 说明s[i,j]的**子串s[i+1][j-1]**也是回文串
+// 说明，i是从最大值开始遍历的，j是从最小值开始遍历的
+// 特殊情况
+// j - i < 2：意即子串是一个长度为0或1的回文串
+// 总结
+// dp[i][j] = s[i] == s[j] && ( dp[i+1][j-1] || j - i < 2)
+
+
 var longestPalindrome = function(s) {
-    function isPalindrome(str){
-        return res.join('') === res.reverse().join('')
-    }
-    let tempStr = ''
-    let maxStr = ''
-    for(let i = 0; i < s.length; i++){
-        tempStr =  tempStr+String[s[i]]
-        if(isPalindrome(tempStr)){
-            maxStr = tempStr.length > tempStr.length ?  tempStr : maxStr
-        }
-        if(maxStr && !isPalindrome(tempStr)){
-            tempStr = s[i]
-        }
-    }
-    return maxStr
+    let len = s.length;
+    let res = ''
+   let dp = Array.from(new Array(len),() => new Array(len).fill(0))
+   for(let i = len -1; i >=0;i--){
+       for(let j=i; j < len; j++){
+           dp[i][j] =  s[i] === s[j] && (j-i<2 || dp[i+1][j-1])
+           if(dp[i][j] && j -i +1 > res.length){
+               res = s.substring(i,j+1)
+           }
+       }
+   }
+   return res
 };
