02837c07d3af196e5585fb54819b8242967fe540
huangxingyuan <uestchxy@163.com>
2021-01-02 23:26:32 +0800

    338-比特位计数-countBits-【medium】

---
+++ top100/338-比特位计数-countBits-【medium】/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: huangxingyuan
+ * @Date: 2021-01-02 23:10:10
+ * @LastEditors: huangxingyuan
+ * @LastEditTime: 2021-01-02 23:26:24
+ * @Description: 文件功能描述
+ */
+var countBits = function(num) {
+    let res = []
+    let n = 0
+    while(n<=num){
+        let matchList = n.toString(2).match(/1/g) || []
+        console.log(' matchList ', matchList )
+        res.push(matchList.length)
+        n++
+    }
+    return res
+
+};
